827d40361d521c617cbc6ddf9838413973dbf813
AravinthanV <webaravindhan@gmail.com>
2017-09-04 00:10:31 +0530

    Create Tallest.java

---
+++ Tallest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Tallest
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int height[]=new int[50];
+    for(int i=0;i<50;i++)
+      height[i]=sc.nextInt();
+    Arrays.sort(height);
+    int K=sc.nextInt();
+    System.out.println(height.length-k+1);
+  }
+}
